74b20289849515fbffb02f205ac5fb31f1b5d7ef
cpowers1203 <81358233+cpowers1203@users.noreply.github.com>
2021-07-27 17:17:41 -0500

    got some code done

---
+++ routes/lists.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const userRouter = require('./users.js')
+const { check, validationResult } = require('express-validator')
+const { csrfProtection, asyncHandler } = require('./utils');
+const db = require('../db/models');
+
+
+
+userRouter.get('/:id(\\d)/list', csrfProtection, asyncHandler(async (req, res) => {
+    userId = parseInt(req.params.userId, 10)
+    const userList = await db.UserRestaurantList.findAll({where: userId})
+    res.render('user-list', {
+        title: "Personal Restaurant List",
+        userList,
+        csrfToken: req.csrfToken()
+    })
+}))
+
+userRouter.post('/list', csrfProtection, asyncHandler(async (req, res) => {
+ 
+    const validatorErrors = validationResult(req)
+
+    if (validatorErrors.isEmpty()) {
+        await list.save();
+        res.redirect('/user')
+    } else {
+    const errorArray = validatorErrors.array()
+      .map(error => error.msg)
+    res.render('user-list', {
+        title: "List",
+        errors: errorArray,
+        csrfToken: req.csrfToken()
+    });
+     
+  }
+}))
