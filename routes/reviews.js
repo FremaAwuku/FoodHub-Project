cdbb4b47751abf746948f6ff9780bcb55d454ac5
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-07-27 15:16:59 -0600

    changed router

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,17 +1,16 @@
 const db = require("../db/models");
 const { asyncHandler } = require("./utils");
-
-const reviewRouter = express.Router();
+const restaurantRouter = require("./restaurant.js")
 
 
 
 //GET collection of reviews written for a specific restaurant
-reviewRouter.get('/restaurants/:id(\\d+)/reivews', asyncHandler(async(req, res) => {
+restaurantRouter.get('/:id(\\d+)/reviews', asyncHandler(async(req, res) => {
     //takes the id for the URL and turns it from a string into an int
     const restaurantId = parseInt(req.params.id, 10);
     const restaurant = await db.Restaurant.findByPk(restaurantId);
     res.render('review', {
-        
+
     })
 }))
 
