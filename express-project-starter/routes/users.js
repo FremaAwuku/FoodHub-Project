0983fceed8c8d25ec5c013d2164accccf2fa832f
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-07-27 09:32:44 -0600

    restructured filed for heroku formatting

--- express-project-starter/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userRouter = express.Router();
-
-/* GET users listing. */
-userRouter.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = userRouter;
