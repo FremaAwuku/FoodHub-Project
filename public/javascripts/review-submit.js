0b6048228b767b222afd50fcc706dd34a71cc1fa
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-07-31 17:19:02 -0600

    ugly fixed the submit buttons

---
+++ public/javascripts/review-submit.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("submit-btn").addEventListener("click", () => {
+        document.getElementById("edit-rev-form").submit()
+    })
+})
