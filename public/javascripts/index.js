34871a07074ea8ffa0bd36e3d0cb1c1954c22875
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-07-31 22:09:41 -0600

    added a cover photo, styled the navbar & splash page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,10 @@
 //const { header } = require("express-validator")
 
-window.addEventListener("load", (event) => {
-    
+window.addEventListener("DOMContentLoaded", () => {
+    let restDiv = Array.from(document.getElementsByClassName("restaurant-container-div"));
 
-
+    for(let i = 1; i < restDiv.length; i+=2) {
+        restDiv[i].classList.add("flipped");
+    }
 
 })
